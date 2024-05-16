c92b8bae2763093ddc486502da4fd43c2051bbd8
ELIEZERSPIKE <amesseliezer@gmail.com>
2024-05-16 11:18:23 +0000

    Mise en place de l'interface d'inscription

--- pages/articles.php
+++ pages/articles.php
@@ -3,16 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <title>BlogADN</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Articles</title>
 </head>
 <body>
-    <?php 
-    require_once './includes/menu.php';
-    
-    
+<?php
+     require_once '../includes/menu.php';
     ?>
-
-
+    
 </body>
 </html>
